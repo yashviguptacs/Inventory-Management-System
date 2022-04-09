cf852c50663de6b5171fad25463d3a0740da16f0
YASHVI GUPTA <71514085+yashviguptacs@users.noreply.github.com>
2022-04-09 21:23:15 +0530

    Add files via upload

---
+++ create_db.py
@@ -0,0 +1,11 @@
+import sqlite3
+def create_db():
+    con=sqlite3.connect(database=r'ims.db')
+    cur=con.cursor()
+    cur.execute("CREATE TABLE IF NOT EXISTS employee(eid INTEGER PRIMARY KEY AUTOINCREMENT, name text , email text , gender text , contact text , dob text, doj text , pass text , utype text , address text , salary text) ")
+    con.commit()
+
+    cur.execute("CREATE TABLE IF NOT EXISTS supplier(invoice INTEGER PRIMARY KEY AUTOINCREMENT, name text , contact text , desc text ) ")
+    con.commit()
+    
+create_db()
